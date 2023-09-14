693b81f02e12171428288cfa18159d38df1159c5
omomowo symeon <omomowosymeon45@gmail.com>
2023-09-14 16:43:34 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -1,28 +1,48 @@
 let navbar = document.querySelector(".navbar");
 let navLinks = document.querySelectorAll(".navlink-lg");
+let brand = document.querySelector(".navbar-brand");
+let iconWhite = document.querySelectorAll(".icon-white");
+let dot = document.querySelector(".rounded-circle");
+let iconDark = document.querySelectorAll(".icon-dark");
 let heroSection = document.querySelector("#home");
 
-// window.addEventListener("load",() => {
-//     ChangeNavbarAppearance();
-// });
+window.addEventListener("load",() => {
+    ChangeNavbarAppearance();
+});
 
-// window.addEventListener("scroll",() => {
-//     ChangeNavbarAppearance();
-// });
+window.addEventListener("scroll",() => {
+    ChangeNavbarAppearance();
+});
 
-// function ChangeNavbarAppearance() {
-//     if (heroSection.getBoundingClientRect().top <= -100) {
-//         navbar.classList.add("bg-white");
-//         navbar.classList.add("shadow-sm");
-//         navLinks.forEach(element => {
-//             element.classList.add("text-dark");
-//         });
-//     } 
-//     else {
-//         navbar.classList.remove("bg-white");
-//         navbar.classList.remove("shadow-sm");
-//         navLinks.forEach(element => {
-//             element.classList.add("text-primary");
-//         });
-//     }
-// };
+function ChangeNavbarAppearance() {
+    if (heroSection.getBoundingClientRect().top <= -100) {
+        navbar.classList.add("bg-white");
+        navbar.classList.add("shadow-sm");
+        brand.classList.replace("text-primary", "text-dark");
+        dot.classList.replace("bg-primary", "bg-dark");
+        navLinks.forEach(element => {
+            element.classList.replace("text-primary", "text-dark");
+        });
+        iconDark.forEach(i => {
+            i.classList.remove("d-none");
+        })
+        iconWhite.forEach(i => {
+            i.classList.add("d-none");
+        })
+    } 
+    else {
+        navbar.classList.remove("bg-white");
+        navbar.classList.remove("shadow-sm");
+        brand.classList.replace("text-dark", "text-primary");
+        dot.classList.replace("bg-dark", "bg-primary");
+        navLinks.forEach(element => {
+            element.classList.replace("text-dark", "text-primary");
+        });
+        iconDark.forEach(i => {
+            i.classList.add("d-none");
+        })
+        iconWhite.forEach(i => {
+            i.classList.remove("d-none");
+        })
+    }
+};
