09c482ad6049e79b5271da17011eb42d623875b7
omomowo symeon <omomowosymeon45@gmail.com>
2023-09-14 14:01:46 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let navbar = document.querySelector(".navbar");
-let navLinks = document.querySelectorAll(".navlink-lg")
+let navLinks = document.querySelectorAll(".navlink-lg");
 let heroSection = document.querySelector("#home");
 
 window.addEventListener("load",() => {
@@ -10,19 +10,19 @@ window.addEventListener("scroll",() => {
     ChangeNavbarAppearance();
 });
 
-function ChangeNavbarAppearance() {
-    if (heroSection.getBoundingClientRect().bottom <= 50) {
-        navbar.classList.add("bg-white");
-        navbar.classList.add("shadow-sm");
-        navLinks.forEach(element => {
-            element.style.color = "#333333";
-        });
-    } 
-    else {
-        navbar.classList.remove("bg-white");
-        navbar.classList.remove("shadow-sm");
-        navLinks.forEach(element => {
-            element.style.color = "#f8f8fa";
-        });
-    }
-};
+// function ChangeNavbarAppearance() {
+//     if (heroSection.getBoundingClientRect().top <= -100) {
+//         navbar.classList.add("bg-white");
+//         navbar.classList.add("shadow-sm");
+//         navLinks.forEach(element => {
+//             element.classList.add("text-dark");
+//         });
+//     } 
+//     else {
+//         navbar.classList.remove("bg-white");
+//         navbar.classList.remove("shadow-sm");
+//         navLinks.forEach(element => {
+//             element.classList.add("text-primary");
+//         });
+//     }
+// };
